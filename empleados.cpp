1337f0b44a361a2a305aa62fdfc31b7aef98335a
femamanic <femamanic@unjbg.edu.pe>
2024-07-10 18:46:44 -0500

    Actualizar empleados.cpp: ahora se imprimen los sueldos

--- empleados.cpp
+++ empleados.cpp
@@ -17,7 +17,7 @@ int main () {
     cout << "Numero de empleados: "; cin >> n;
     for (int i = 0; i < n; i++) {
         system("cls");
-        cout << "Empleado " << i+1 << "\n";
+        cout << "EMPLEADO " << i+1 << "\n";
         cout << "Numero: "; cin >> EMPLEADO[i].numero;
         cin.ignore();
         cout << "Nombre: "; getline(cin,EMPLEADO[i].nombre);
@@ -38,31 +38,41 @@ int main () {
             pos = i;
         }
     }
+    system("cls");
     cout << "Empleado con mayor venta anual:\n" << EMPLEADO[pos].numero << " " <<
             EMPLEADO[pos].nombre << "\n";
+    cout << "===============================\n";
+    cout << "Salarios:\n";
+    cout << "-------------------------------\n";
     for (int i = 0; i < n; i++){
+        cout << "Empleado " << EMPLEADO[i].numero << " " << EMPLEADO[i].nombre << "\n";
+        cout << "Salario: " << EMPLEADO[i].sueldo << "\n";
         int vent = 0;
         for (int j = 0; j < 12; j++){
             vent = vent + EMPLEADO[i].ventas[j];
         }
         if (vent > 100){
             EMPLEADO[i].sueldo = EMPLEADO[i].sueldo + (0.1 * EMPLEADO[i].sueldo);
+            cout << "Nuevo salario: " << EMPLEADO[i].sueldo << "\n";
         }
+        if (i < n-1) cout << "-------------------------------\n";
+        else cout << "===============================\n";
     }
-    cout << EMPLEADO[0].sueldo << EMPLEADO[1].sueldo << EMPLEADO[2].sueldo << "\n";
     int N[50], j = 0;
     string D[50];
     for (int i = 0; i < n; i++){
         int vdic = 0;
-        vdic = EMPLEADO[i].ventas[12];
+        vdic = EMPLEADO[i].ventas[11];
         if (vdic < 30){
             N[j] = EMPLEADO[i].numero;
             D[j] = EMPLEADO[i].nombre;
             j++;
         }
     }
+    cout << "Empleados con ventas menores a 30 en diciembre:\n";
     for (int i = 0; i < j; i++){
-        cout << "Empleado con ventas menores a 30 en diciembre: " << N[i] << D[i] << "\n";
+        cout << N[i] << " " << D[i] << "\n";
     }
+    system("pause>nul");
     return 0;
 }
