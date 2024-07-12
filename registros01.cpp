d40302027b4d5019f3c351581bed02fea62be820
femamanic <femamanic@unjbg.edu.pe>
2024-07-12 18:44:41 -0500

    Actualizar registros01.cpp: imprime los datos del empleado con mayor y menor sueldo

--- registros01.cpp
+++ registros01.cpp
@@ -17,12 +17,30 @@ int main () {
         system ("cls");
         cout << "\tDataTech\n";
         cout << "==============================\n";
-        cout << "EMPLEADO " << i+1 << "\n";
+        cout << "Empleado " << i+1 << ":\n";
         cin.ignore();
         cout << "Nombre: "; getline(cin, empleados[i].nombre);
         cout << "Sexo: "; cin >> empleados[i].sexo;
         cout << "Sueldo: "; cin >> empleados[i].sueldo;
     }
+    int posmax = 0, posmin = 0;
+    for (int i = 0; i < n; i++) {
+        if (empleados[i].sueldo > empleados[posmax].sueldo) posmax = i;
+        if (empleados[i].sueldo < empleados[posmin].sueldo) posmin = i;
+    }
+    system ("cls"); 
+    cout << "\t   DataTech\n";
+    cout << "==============================\n";
+    cout << "Empleado con mayor sueldo:\n";
+    cout << "Nombre: " << empleados[posmax].nombre << "\n";
+    cout << "Sexo: " << empleados[posmax].sexo << "\n";
+    cout << "Sueldo: " << empleados[posmax].sueldo << "\n";
+    cout << "------------------------------\n";
+    cout << "Empleado con menor sueldo:\n";
+    cout << "Nombre: " << empleados[posmin].nombre << "\n";
+    cout << "Sexo: " << empleados[posmin].sexo << "\n";
+    cout << "Sueldo: " << empleados[posmin].sueldo << "\n";
+    system ("pause>nul");
     system ("cls");
     for (int i = 0; i < n; i++) {
         cout << "Empleado " << i+1 << "\n";
@@ -31,4 +49,6 @@ int main () {
         cout << "Sueldo: " << empleados[i].sueldo << "\n";
         if (i < n-1) cout << "------------------------------\n";
     }
+    system ("pause>nul");
+    return 0;
 }
