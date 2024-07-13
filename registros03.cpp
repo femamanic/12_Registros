22dc5233d49760451cecf5ce74ea701e7d8b5fec
femamanic <femamanic@unjbg.edu.pe>
2024-07-13 10:00:32 -0500

    Actualizar registros03.cpp: la fecha debe ser valida

--- registros03.cpp
+++ registros03.cpp
@@ -10,6 +10,7 @@ struct fecha {
 
 int main () {
     int n;
+    bool error = false;
     system ("cls");
     cout << "Numero de personas: "; cin >> n;
     for (int i = 0; i < n; i++) {
@@ -17,7 +18,35 @@ int main () {
         cout << "Persona " << i+1 << ":\n";
         cin.ignore();
         cout << "Nombres: "; getline(cin, fecha[i].nombres);
-        cout << "Fecha de nacimiento (dd/mm/aaaa): "; cin >> fecha[i].dia >> fecha[i].mes >> fecha[i].ano;
+        do {
+            cout << "Fecha de nacimiento (dd/mm/aaaa): "; cin >> fecha[i].dia >> fecha[i].mes >> fecha[i].ano;
+            if (fecha[i].mes == 1 || fecha[i].mes == 3 || fecha[i].mes == 5 || fecha[i].mes == 7 || fecha[i].mes == 8 || fecha[i].mes == 10 || fecha[i].mes == 12) {
+                if (fecha[i].dia < 1 || fecha[i].dia > 31) {
+                    cout << "\t[Error: dia invalido]\n";
+                    error = true;
+                } else error = false;
+            } else if (fecha[i].mes == 4 || fecha[i].mes == 6 || fecha[i].mes == 9 || fecha[i].mes == 11) {
+                if (fecha[i].dia < 1 || fecha[i].dia > 30) {
+                    cout << "\t[Error: dia invalido]\n";
+                    error = true;
+                } else error = false;
+            } else if (fecha[i].mes == 2) {
+                if ((fecha[i].ano % 4 == 0 && fecha[i].ano % 100 != 0) || fecha[i].ano % 400 == 0) {
+                    if (fecha[i].dia < 1 || fecha[i].dia > 29) {
+                        cout << "\t[Error: dia invalido]\n";
+                        error = true;
+                    } else error = false;
+                } else {
+                    if (fecha[i].dia < 1 || fecha[i].dia > 28) {
+                        cout << "\t[Error: dia invalido]\n";
+                        error = true;
+                    } else error = false;
+                }
+            } else {
+                cout << "\t[Error: mes invalido]\n";
+                error = true;
+            }
+        } while (error == true);
     }
     system ("cls");
     cout << "Personas registradas\n";
