40533ca588990024c84f69b8d79865146e6a2d94
femamanic <femamanic@unjbg.edu.pe>
2024-07-14 13:13:33 -0500

    Corregir registros02.cpp: nombres de variables

--- registros02.cpp
+++ registros02.cpp
@@ -1,56 +1,56 @@
 #include <iostream>
 using namespace std;
 
-struct persona {
+struct personas {
     string nombres;
     string DNI;
     int edad;
-} personas[50];
+} persona[50];
 
 int main () {
     system ("cls");
-    int n; float promEdad, sEdad;
+    int n; float promEdad, sEdad = 0;
     cout << "Numero de personas: "; cin >> n;
     for (int i = 0; i < n; i++) {
         system ("cls");
         cout << "Persona " << i+1 << ":\n";
         cin.ignore();
-        cout << "Nombres: "; getline(cin, personas[i].nombres);
+        cout << "Nombres: "; getline(cin, persona[i].nombres);
         do {
-            cout << "DNI: "; cin >> personas[i].DNI;
-            if (personas[i].DNI.length() != 8){
+            cout << "DNI: "; cin >> persona[i].DNI;
+            if (persona[i].DNI.length() != 8){
                 cout << "\t[Error: DNI invalido]\n";
             }
-        } while (personas[i].DNI.length() != 8);
+        } while (persona[i].DNI.length() != 8);
         do {
-            cout << "Edad: "; cin >> personas[i].edad;
-            if (personas[i].edad < 0) {
+            cout << "Edad: "; cin >> persona[i].edad;
+            if (persona[i].edad < 0) {
                 cout << "\t[Error: edad invalida]\n";
             }
-        } while (personas[i].edad < 0);
-        sEdad += personas[i].edad;
+        } while (persona[i].edad < 0);
+        sEdad += persona[i].edad;
     }
-    int mayor = 0; float promayor = 0 , Pmayor;
+    int nMayor = 0; float sMayor = 0 , promMayor;
     for (int i = 0; i < n; i++) {
-        if (personas[i].edad > 50) {
-            mayor++;
-            promayor += personas[i].edad;
+        if (persona[i].edad > 50) {
+            nMayor++;
+            sMayor += persona[i].edad;
         }
     }
     system ("cls");
-    cout << "Personas con una edad mayor a 50: " << mayor << "\n";
+    cout << "Personas con una edad mayor a 50: " << nMayor << "\n";
     cout << "==============================\n";
     for (int i = 0; i < n; i++) {
-        if (personas[i].edad > 50) {
-            cout << "Nombres: " << personas[i].nombres << "\n";
-            cout << "DNI: " << personas[i].DNI << "\n";
-            cout << "Edad: " << personas[i].edad << "\n";
+        if (persona[i].edad > 50) {
+            cout << "Nombres: " << persona[i].nombres << "\n";
+            cout << "DNI: " << persona[i].DNI << "\n";
+            cout << "Edad: " << persona[i].edad << "\n";
             if (i < n-1) cout << "------------------------------\n";
             else cout << "==============================\n";
         }
     }
-    Pmayor = promayor/mayor;
-    cout << "Promedio de edad de personas con edad mayor a 50: " << Pmayor << "\n";
+    promMayor = sMayor/nMayor;
+    cout << "Promedio de edad de personas con edad mayor a 50: " << promMayor << "\n";
     system ("pause>nul");
     system ("cls");
     cout << "     Personas registradas\n";
@@ -59,9 +59,9 @@ int main () {
     cout << "Promedio de edad: " << promEdad << "\n";
     cout << "==============================\n";
     for (int i = 0; i < n; i++) {
-        cout << "Nombres: " << personas[i].nombres << "\n";
-        cout << "DNI: " << personas[i].DNI << "\n";
-        cout << "Edad: " << personas[i].edad << "\n";
+        cout << "Nombres: " << persona[i].nombres << "\n";
+        cout << "DNI: " << persona[i].DNI << "\n";
+        cout << "Edad: " << persona[i].edad << "\n";
         if (i < n-1) cout << "------------------------------\n";
     }
     system ("pause>nul");
