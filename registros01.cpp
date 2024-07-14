112dac181e0e4ecd90aa29544ed099d264ae1d89
femamanic <femamanic@unjbg.edu.pe>
2024-07-14 18:39:41 -0500

    Actualizar registros01.cpp

--- registros01.cpp
+++ registros01.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-struct empleado {
+struct empleados {
     string nombre;
     string sexo;
     float sueldo;
-} empleados[50];
+} empleado[50];
 
 int main () {
     system ("cls");
@@ -19,36 +19,36 @@ int main () {
         cout << "==============================\n";
         cout << "Empleado " << i+1 << ":\n";
         cin.ignore();
-        cout << "Nombre: "; getline(cin, empleados[i].nombre);
-        cout << "Sexo: "; cin >> empleados[i].sexo;
-        cout << "Sueldo: "; cin >> empleados[i].sueldo;
+        cout << "Nombre: "; getline(cin, empleado[i].nombre);
+        cout << "Sexo: "; cin >> empleado[i].sexo;
+        cout << "Sueldo: "; cin >> empleado[i].sueldo;
     }
     int posmax = 0, posmin = 0;
     for (int i = 0; i < n; i++) {
-        if (empleados[i].sueldo > empleados[posmax].sueldo) posmax = i;
-        if (empleados[i].sueldo < empleados[posmin].sueldo) posmin = i;
+        if (empleado[i].sueldo > empleado[posmax].sueldo) posmax = i;
+        if (empleado[i].sueldo < empleado[posmin].sueldo) posmin = i;
     }
     system ("cls"); 
     cout << "\t   DataTech\n";
     cout << "==============================\n";
     cout << "Empleado con mayor sueldo:\n";
-    cout << "Nombre: " << empleados[posmax].nombre << "\n";
-    cout << "Sexo: " << empleados[posmax].sexo << "\n";
-    cout << "Sueldo: " << empleados[posmax].sueldo << "\n";
+    cout << "Nombre: " << empleado[posmax].nombre << "\n";
+    cout << "Sexo: " << empleado[posmax].sexo << "\n";
+    cout << "Sueldo: " << empleado[posmax].sueldo << "\n";
     cout << "------------------------------\n";
     cout << "Empleado con menor sueldo:\n";
-    cout << "Nombre: " << empleados[posmin].nombre << "\n";
-    cout << "Sexo: " << empleados[posmin].sexo << "\n";
-    cout << "Sueldo: " << empleados[posmin].sueldo << "\n";
+    cout << "Nombre: " << empleado[posmin].nombre << "\n";
+    cout << "Sexo: " << empleado[posmin].sexo << "\n";
+    cout << "Sueldo: " << empleado[posmin].sueldo << "\n";
     system ("pause>nul");
     system ("cls");
     cout << "\t   DataTech\n";
     cout << "==============================\n";
     for (int i = 0; i < n; i++) {
         cout << "Empleado " << i+1 << "\n";
-        cout << "Nombre: " << empleados[i].nombre << "\n";
-        cout << "Sexo: " << empleados[i].sexo << "\n";
-        cout << "Sueldo: " << empleados[i].sueldo << "\n";
+        cout << "Nombre: " << empleado[i].nombre << "\n";
+        cout << "Sexo: " << empleado[i].sexo << "\n";
+        cout << "Sueldo: " << empleado[i].sueldo << "\n";
         if (i < n-1) cout << "------------------------------\n";
     }
     system ("pause>nul");
