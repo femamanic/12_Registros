4d53e2dbfe1c46c020c901aa0cf83634df5a8610
femamanic <femamanic@unjbg.edu.pe>
2024-07-13 19:06:26 -0500

    Actualizar registros04.cpp: imprime los datos del atleta con mayor número de medallas de un país en especifico

--- registros04.cpp
+++ registros04.cpp
@@ -10,7 +10,7 @@ struct atletas {
 
 int main () {
     system ("cls");
-    int n;
+    int n, masM, posM;
     string p;
     cout << "Numero de atletas: "; cin >> n;
     cout << "==============================\n";
@@ -25,16 +25,31 @@ int main () {
     }
     system ("cls");
     cout << "Ingresa el pais para buscar atletas: "; cin >> p;
+    masM = atleta[0].nMedallas;
+    posM = 0;
+    for (int i = 1; i < n; i++) {
+        if (atleta[i].pais == p) {
+            if (atleta[i].nMedallas > masM) {
+                masM = atleta[i].nMedallas;
+                posM = i;
+            }
+        }
+    }
     system ("cls");
-    cout << "Atletas de " << p << "\n";
-    cout << "==============================\n";
+    cout << "   Atleta con mas medallas de " << p << "\n";
+    cout << "Nombres: " << atleta[posM].nombres << "\n";
+    cout << "Pais: " << atleta[posM].pais << "\n";
+    cout << "Disiplina: " << atleta[posM].disiplina << "\n";
+    cout << "Numero de medallas: " << atleta[posM].nMedallas << "\n";
+    cout << "========================================\n";
+    cout << "   Registro de atletas de " << p << ":\n";
     for (int i = 0; i < n; i++) {
         if (atleta[i].pais == p) {
             cout << "Nombres: " << atleta[i].nombres << "\n";
             cout << "Pais: " << atleta[i].pais << "\n";
             cout << "Disiplina: " << atleta[i].disiplina << "\n";
             cout << "Numero de medallas: " << atleta[i].nMedallas << "\n";
-            cout << "------------------------------\n";
+            cout << "----------------------------------------\n";
         }
     }
     return 0;
